3de284b53e2a0e3eb80376029bf0b7c45a4224f1
Milena Bilewska <bilewskamilena@gmail.com>
2023-11-25 09:21:42 +0100

    Initial commit

---
+++ src/main/java/org/example/Enkapsulacja/Main.java
@@ -0,0 +1,24 @@
+package org.example.Enkapsulacja;
+
+public class Main {
+    public static void main(String[] args) {
+        Osoba osoba = new Osoba("Jan", "Kowalski" , 22);
+
+        System.out.println("Imie: "   + osoba.getImie());
+        System.out.println("Nazwisko: "   + osoba.getNazwisko());
+        System.out.println("Wiek: "   + osoba.getWiek());
+
+
+        osoba.setWiek(33);
+        System.out.println("Nowy wiek to: "   + osoba.getWiek());
+        osoba.setImie("Anna");
+        osoba.setNazwisko("Nowak");
+
+
+        System.out.println(osoba.getImie() + " " + osoba.getNazwisko() + " " + osoba.getWiek());
+
+
+
+
+    }
+}
